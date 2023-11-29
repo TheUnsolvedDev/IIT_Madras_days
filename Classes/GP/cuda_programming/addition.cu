33e02c26b7b714e60192bc85c8266020f3cf53c6
Shuvrajeet Das <shuvrajeet17@gmail.com>
2023-11-29 23:23:20 +0530

    updates

---
+++ Classes/GP/cuda_programming/addition.cu
@@ -0,0 +1,34 @@
+#include <cuda.h>
+#include <stdio.h>
+
+__global__ void add(int *a, int *b, int *c)
+{
+	*c = *a + *b;
+}
+
+int main()
+{
+	int a, b, c;
+	int *d_a, *d_b, *d_c;
+	int size = sizeof(int);
+
+	cudaMalloc(&d_a, size);
+	cudaMalloc(&d_b, size);
+	cudaMalloc(&d_c, size);
+
+	a = 2, b = 102;
+
+	cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
+
+	add<<<1, 1>>>(d_a, d_b, d_c);
+	cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+
+	printf("The result is %d\n", c);
+
+	return 0;
+}
