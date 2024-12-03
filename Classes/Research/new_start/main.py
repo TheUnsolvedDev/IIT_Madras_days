d3e0d9f5a78893ef208c0968a9031dde237255d6
Shuvrajeet Das <shuvrajeet17@gmail.com>
2024-12-03 16:28:33 +0530

    updates

--- Classes/Research/new_start/main.py
+++ Classes/Research/new_start/main.py
@@ -9,9 +9,9 @@ from utils import *
 
 
 class Algorithm:
-    def __init__(self, max_iteration=1000) -> None:
+    def __init__(self, max_iteration=100) -> None:
         self.max_iteration = max_iteration
-        self.env = Env1D()
+        self.env = Env1D(size=30)
         self.solver = Solver(max_iteration)
         self.all_mask_coord = [
             (x, y) for x in range(self.env.size) for y in range(self.env.size)]
@@ -22,26 +22,38 @@ class Algorithm:
         self.theta_star = self.env.reset()
         self.A, self.b = [], []
 
-    def simulate(self):
+    def simulate(self, kernel='sharpen'):
         theta = jnp.zeros_like(self.theta_star)
+        rank_history = []
         action = 10
-        for i in range(self.max_iteration):
+        for i in range(self.max_iteration+1):
             mask, result = self.env.act(action)
             self.A.append(mask)
             self.b.append(result)
-            theta, prod = self.solver.solve_LOG_kernel(self.A, self.b, 0.01)
-            theta += jax.random.normal(self.env.rng, (self.env.size,))*0.01
+            if kernel == 'sharpen':
+                theta, prod = self.solver.solve_Sharpen_kernel(
+                    self.A, self.b, 0.01)
+            elif kernel == 'log':
+                theta, prod = self.solver.solve_LOG_kernel(
+                    self.A, self.b, 0.01)
+            elif kernel == 'tikhonov':
+                theta, prod = self.solver.solve_tikhonov(self.A, self.b, 0.01)
+            theta += jax.random.normal(self.env.rng, (self.env.size,))*(1e-4)
             theta = jnp.clip(theta, 0, 1)
 
-            if i % 1 == 0:
-                print('action taken:', idx_to_coord(action, self.env.size)[
-                      0], idx_to_coord(action, self.env.size)[1],'rank:',jnp.linalg.matrix_rank(jnp.array(self.A)))
-                hmap = np.vstack([mask, theta, self.theta_star])
+            rank = jnp.linalg.matrix_rank(jnp.array(self.A))
+            rank_history.append(rank)
+            
+            if i % 20 == 0:
+                print('steps:', i, '\taction taken:', idx_to_coord(action, self.env.size)[
+                      0], idx_to_coord(action, self.env.size)[1], '\trank:', rank)
+                hmap = np.vstack([mask, self.theta_star, theta])
                 plt.imshow(hmap)
                 plt.show()
 
             eigen_values, eigen_vectors = jnp.linalg.eigh(prod)
-            action = jnp.argmax(self.all_mask@eigen_vectors[0])
+            action = jnp.argmin(self.all_mask@eigen_vectors[0])
+        return rank_history
 
 
 if __name__ == "__main__":
--- Classes/Research/new_start/solver.py
+++ Classes/Research/new_start/solver.py
@@ -16,15 +16,24 @@ class Solver:
     def __init__(self, max_iteration=1000) -> None:
         self.max_iteration = max_iteration
 
-    def solve_tikhonov(self, A, b, lambda_):
+    def solve_tikhonov(self, A, b, lambda_):        
         A, b = jnp.array(A), jnp.array(b)
         return jnp.linalg.pinv(A.T @ A + lambda_ * jnp.eye(A.shape[1])) @ A.T @ b, A.T @ A
     
-    def solve_LOG_kernel(self, A, b, prior_value=0.1):
+    def solve_LOG_kernel(self, A, b, prior_value=0.01):
         A, b = jnp.array(A), jnp.array(b)
         theta_prior = jnp.ones(A.shape[1]) * prior_value
         new_A = conv_parallel(A, kernel=jnp.array([1,-2,1]))
-        term1 = jnp.linalg.pinv(new_A.T @ new_A) + theta_prior
+        term1 = jnp.linalg.pinv(new_A.T @ new_A + theta_prior) 
+        term2 = jnp.dot(A.T,b.reshape(-1,1)) 
+        result = jnp.dot(term1, term2)
+        return result.reshape((-1,)), term1
+    
+    def solve_Sharpen_kernel(self, A, b, prior_value=0.01):
+        A, b = jnp.array(A), jnp.array(b)
+        theta_prior = jnp.ones(A.shape[1]) * prior_value
+        new_A = conv_parallel(A, kernel=jnp.array([-1,4,-1]))
+        term1 = jnp.linalg.pinv(new_A.T @ new_A + theta_prior) 
         term2 = jnp.dot(A.T,b.reshape(-1,1)) 
         result = jnp.dot(term1, term2)
         return result.reshape((-1,)), term1
